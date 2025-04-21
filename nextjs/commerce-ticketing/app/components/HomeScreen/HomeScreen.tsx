1f898605b2c0395f7cb1d70d7485fd50a70180c7
Fleur N’GUESSAN <ngfleur01@gmail.com>
2025-04-21 21:57:55 +0200

    Commit après avoir cassé le projet

--- nextjs/commerce-ticketing/app/components/HomeScreen/HomeScreen.tsx
+++ nextjs/commerce-ticketing/app/components/HomeScreen/HomeScreen.tsx
@@ -1,26 +1,44 @@
 // app/components/HomeScreen/HomeScreen.tsx
+'use client';
 import { Evenement } from '@app/types/evenement';
-import { formatDate } from '@app/utils/dateUtils';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface HomeScreenProps {
   evenements: Evenement[];
 }
 
-export function HomeScreen({ evenements }: HomeScreenProps) {
+interface EventState {
+  [key: number]: boolean;
+}
+
+export const HomeScreen = ({ evenements }: { evenements: Evenement[] }) => {
+  const [openEvents, setOpenEvents] = useState<EventState>({});
+
+  const toggleEvent = (id: number) => {
+    setOpenEvents((prev: EventState) => ({
+      // Ajout du type EventState ici
+      ...prev,
+      [id]: !prev[id]
+    }));
+  };
+
   return (
     <main className="bg-site min-h-[600px]">
       <div className="mx-auto relative">
         <div className="relative">
           <div className="flex sm:flex-row flex-col bg-zinc-900">
             <div className="basis-1/2 text-center sm:text-left relative">
-              <div className="px-10 sm:px-14 py-6 bg-site" data-testid="home.header">
+              <div
+                className="px-10 sm:px-14 py-6 bg-site"
+                data-testid="home.header"
+              >
                 <h1 className="text-5xl sm:text-[120px] leading-none animate-fade-in">
                   VOS
-                  <br/>
+                  <br />
                   BILLETS
-                  <br/>
+                  <br />
                   ICI
                 </h1>
                 <h3 className="text-base sm:text-2xl py-6">
@@ -51,8 +69,13 @@ export function HomeScreen({ evenements }: HomeScreenProps) {
 
         {/* Section des événements */}
         <div className="bg-zinc-900 text-site pt-16 sm:p-20">
-          <div className="px-8 sm:px-0 max-w-4xl mx-auto" data-testid="tickets.container">
-            <h1 className="uppercase text-4xl sm:text-7xl text-center sm:text-left">SHOWS</h1>
+          <div
+            className="px-8 sm:px-0 max-w-4xl mx-auto"
+            data-testid="tickets.container"
+          >
+            <h1 className="uppercase text-4xl sm:text-7xl text-center sm:text-left">
+              SHOWS
+            </h1>
             <div className="py-10">
               {evenements.map((evenement) => (
                 <div
@@ -61,9 +84,11 @@ export function HomeScreen({ evenements }: HomeScreenProps) {
                   className="group/event flex border-b last:border-0 hover:border-purple-500 gap-4 sm:gap-8 flex-col sm:flex-row py-4 sm:py-0 transition-colors duration-300"
                 >
                   <div className="flex flex-1 sm:items-center gap-4 sm:gap-4 md:gap-8 flex-col sm:flex-row sm:py-3">
-                    <div className="sm:group-hover/event:overflow-hidden transition-all duration-300 ease-out w-full max-w-fit sm:group-hover/event:w-0">
+                    <div
+                      className="sm:group-hover/event:overflow-hidden transition-all duration-300 ease-out w-full max-w-fit sm:group-hover/event:w-0">
                       <div className="flex flex-col min-w-fit sm:flex-row overflow-hidden sm:gap-4 md:gap-8 relative">
-                        <div className="w-[310px] h-[171px] sm:w-[80px] sm:h-[80px] overflow-hidden sm:group-hover/event:opacity-0 transition-opacity duration-300">
+                        <div
+                          className="w-[310px] h-[171px] sm:w-[80px] sm:h-[80px] overflow-hidden sm:group-hover/event:opacity-0 transition-opacity duration-300">
                           <div className="flex items-center justify-center h-full">
                             <div className="overflow-hidden relative group w-full h-full">
                               <Image
@@ -76,23 +101,36 @@ export function HomeScreen({ evenements }: HomeScreenProps) {
                             </div>
                           </div>
                         </div>
-                        <div className="flex gap-4 items-center absolute bottom-2 left-2 sm:bottom-auto sm:left-auto sm:relative">
+                        <div
+                          className="flex gap-4 items-center absolute bottom-2 left-2 sm:bottom-auto sm:left-auto sm:relative">
                           <span className="text-4xl">
-                            {new Date(evenement.dateDebut).getDate().toString().padStart(2, '0')}
+                            {new Date(evenement.dateDebut)
+                              .getDate()
+                              .toString()
+                              .padStart(2, '0')}
                           </span>
                           <div className="flex flex-col text-xs">
                             <span className="text-white sm:text-gray-600">
-                              {new Date(evenement.dateDebut).toLocaleDateString('fr-FR', { weekday: 'short' })}
+                              {new Date(evenement.dateDebut).toLocaleDateString(
+                                'fr-FR',
+                                { weekday: 'short' }
+                              )}
                             </span>
                             <span>
-                              {new Date(evenement.dateDebut).toLocaleDateString('fr-FR', { month: 'short' })}
+                              {new Date(evenement.dateDebut).toLocaleDateString(
+                                'fr-FR',
+                                { month: 'short' }
+                              )}
                             </span>
                           </div>
                         </div>
                       </div>
                     </div>
                     <div className="grow flex flex-col hover:text-purple-500">
-                      <button className="text-left w-full h-full group/button text-2xl text-left">
+                      <button
+                        onClick={() => toggleEvent(evenement.id)}
+                        className="text-left w-full h-full group/button text-2xl text-left"
+                      >
                         {evenement.titre}
                         <svg
                           fill="none"
@@ -103,11 +141,34 @@ export function HomeScreen({ evenements }: HomeScreenProps) {
                           xmlns="http://www.w3.org/2000/svg"
                           aria-hidden="true"
                         >
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"></path>
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                          ></path>
                         </svg>
                       </button>
-                      <div className="text-sm text-site transition-all ease-in pointer-events-none opacity-0 h-0">
-                        <p>{formatDate(evenement.dateDebut)}</p>
+                      <div
+                        className={`text-sm text-gray-600 transition-all ease-in ${
+                          openEvents[evenement.id]
+                            ? 'pointer-events-auto opacity-100 h-auto py-3'
+                            : 'pointer-events-none opacity-0 h-0'
+                        }`}
+                      >
+                        <p>
+                          {evenement.dateDebut.toLocaleString('fr-FR', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                            hour: '2-digit',
+                            minute: '2-digit'
+                          })}{' '}
+                          –{' '}
+                          {evenement.dateFin.toLocaleString('fr-FR', {
+                            hour: '2-digit',
+                            minute: '2-digit'
+                          })}
+                        </p>
                         <p>{evenement.lieu}</p>
                         <p className="mt-3">{evenement.description}</p>
                       </div>
@@ -128,4 +189,4 @@ export function HomeScreen({ evenements }: HomeScreenProps) {
       </div>
     </main>
   );
-}
+};
